752d5f858600a10ca53517eb31d80adc6f1ce6af
marie-phuqui <marie.nguyen.phu.qui@gmail.com>
2020-01-28 20:26:09 +0100

    update the movement with also functionning but harder movements

--- Racing_from_scratch_no_RL/Assets/Scripts/PlayerMovement.cs
+++ Racing_from_scratch_no_RL/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    // INITIAL MOVEMENT ACTION
     //movement speed in units per second
     private float movementSpeed = 10f;
     float y;
-    void Update()
+    void FixedUpdate()
     {
         //get the Input from Horizontal axis
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -19,12 +21,45 @@ public class PlayerMovement : MonoBehaviour
         //update the rotate angle
         y += Time.deltaTime * horizontalInput * 200f;
         transform.rotation = Quaternion.Euler(0, y, 0);
-
-        //output to log the position change
-        Debug.Log(transform.position);
-        Debug.Log(transform.rotation);
     }
-    public void PlayerReset()
+
+    /*
+    // new movement action
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        if (Input.GetKey("right"))
+        {
+            rotateDir = transform.up * 1f;
+            transform.Rotate(rotateDir, Time.deltaTime * 200f);
+        }
+        if (Input.GetKey("up"))
+        {
+            dirToGo = transform.forward * 1f;
+            rb.AddForce(dirToGo * 2f, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("left"))
+        {
+            rotateDir = transform.up * -1f;
+            transform.Rotate(rotateDir, Time.deltaTime * 200f);
+        }
+        if (Input.GetKey("down"))
+        {
+            dirToGo = transform.forward * -1f;
+            rb.AddForce(dirToGo * 2f, ForceMode.VelocityChange);
+        }
+    }
+    */
+
+    void PlayerReset()
     {
         //whenever done()
         transform.position = new Vector3(0, 0.5f, -4);
