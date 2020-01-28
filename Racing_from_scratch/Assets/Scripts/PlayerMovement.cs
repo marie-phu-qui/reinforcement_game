3e8aa2636e6ab87f279c15299fd400a3f204973f
marie-phuqui <marie.nguyen.phu.qui@gmail.com>
2020-01-28 16:15:04 +0100

    add possible WebGL build

---
+++ Racing_from_scratch/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //movement speed in units per second
+    private float movementSpeed = 5f;
+
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0f, verticalInput * movementSpeed * Time.deltaTime);
+
+        //output to log the position change
+        Debug.Log(transform.position);
+    }
+    public void PlayerReset()
+    {
+        //whenever done()
+        transform.position = new Vector3(0, 0.5f, -4);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("goal"))
+        {
+            PlayerReset();
+        }
+    }
+}
---
+++ Racing_from_scratch_no_RL/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //movement speed in units per second
+    private float movementSpeed = 10f;
+    float y;
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0f, verticalInput * movementSpeed * Time.deltaTime);
+        //update the rotate angle
+        y += Time.deltaTime * horizontalInput * 200f;
+        transform.rotation = Quaternion.Euler(0, y, 0);
+
+        //output to log the position change
+        Debug.Log(transform.position);
+        Debug.Log(transform.rotation);
+    }
+    public void PlayerReset()
+    {
+        //whenever done()
+        transform.position = new Vector3(0, 0.5f, -4);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("goal"))
+        {
+            PlayerReset();
+        }
+    }
+}
